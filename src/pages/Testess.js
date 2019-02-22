b6c9faac8d8e79ba4a396114940ae4f60ac9193b
SnowLe <38509926+SnowLew@users.noreply.github.com>
2019-02-22 17:46:53 -0300

    Update Testess.js

--- src/pages/Testess.js
+++ src/pages/Testess.js
@@ -1,27 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image,ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, Image,ImageBackground,ActivityIndicator} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 
 
 import ProgressCircle from 'react-native-progress-circle'
 
 
-
-
 export default class Testess extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      showMe:true;
+    }
+  }
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count+10
+    })
+  }
+
+  componentWillMount(){
+
+    setTimeout(()=>{
+      this.setState({
+        showMe:false
+      })
+    },
+    3000)
+  }
+
   render() {
     return (
-      
-      <ProgressCircle
-            percent={30}
-            radius={50}
-            borderWidth={8}
-            color="#3399FF"
-            shadowColor="#999"
-            bgColor="#fff"
-        >
-            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
-        </ProgressCircle>
+       <View style={{flex:1,marginTop:50}}>
+        {
+          this.state.showMe ? 
+          <ActivityIndicator size="large" color="blue"/>
+          : <View>
+              <Text> Carregador carregado</Text>
+            </View>
+        }
+      </View>
+  
+
+
     );
   }
 }
