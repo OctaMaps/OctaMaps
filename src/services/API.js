db3d1942a4f235e47d05c1b468b3ff36e92dc28c
TeijiW <teijiwatanabe01@gmail.com>
2019-07-02 03:03:01 -0400

    Correções nas requisições para a API

--- src/services/API.js
+++ src/services/API.js
@@ -1,32 +1,35 @@
-import axios from 'axios'
+import axios from "axios"
 const credentials = require("../credentials/credentials.json")
 
-function API(){
-  getData = async ( url ) => {
-    try{
-      const response = await axios.get(url)
-      if (response.data.result){
-        return response.data.result
-      }else{
-        return response.data
-      }
-    }catch(error){
-      return new Error(error)
-    }
-  }
+function API() {
+	getData = async url => {
+		try {
+			const response = await axios.get(url)
+			if (response.data.result) {
+				return response.data.result
+			} else {
+				return response.data
+			}
+		} catch (error) {
+			throw error
+		}
+	}
 
-  setData = async (downloadData = true, downloadVersionCode = true) => {
-    try{
-      data = []
-      versionCode = ""
-      if (downloadData) data = await getData(credentials.getDataURL)
-      if (downloadVersionCode) versionCode = await getData(credentials.getUpdateURL)
-      return{ data,versionCode }
-    }catch(error){
-      throw new Error(error)
-    }
-  }
-  return{ setData }
+	setData = async (downloadData = true, downloadVersionCode = true) => {
+		try {
+			data = []
+			versionCode = ""
+			if (downloadData) data = await getData(credentials.getDataURL)
+			if (downloadVersionCode) {
+				const { id_version } = await getData(credentials.getUpdateURL)
+				return { data, versionCode: id_version }
+			}
+			return { data, versionCode }
+		} catch (error) {
+			throw error
+		}
+	}
+	return { setData }
 }
 
 module.exports = API
--- src/services/Database.js
+++ src/services/Database.js
@@ -1,141 +1,156 @@
 import PouchDB from "pouchdb-react-native"
-const API = require('./API')
-const arraySync = require('../utils/arraySync')
+const api = require("./API")
+const arraySync = require("../utils/arraySync")
 
-function Database(name){
+function Database(name) {
+	const database = new PouchDB(name)
+	const apiConn = api()
 
-  const database = new PouchDB(name)
-  const APIConn = API()
+	destroy = async () => {
+		database.destroy()
+	}
 
-  destroy = async () => {
-    database.destroy()
-  }
+	insert = async ({
+		_id,
+		codigo_sala,
+		numero_piso,
+		titulo_bloco,
+		titulo_campus,
+		titulo_sala,
+		_rev
+	}) => {
+		if (_rev) {
+			response = await database.put({
+				_id,
+				_rev,
+				codigo_sala,
+				numero_piso,
+				titulo_bloco,
+				titulo_campus,
+				titulo_sala
+			})
+		} else {
+			await database.put({
+				_id,
+				codigo_sala,
+				numero_piso,
+				titulo_bloco,
+				titulo_campus,
+				titulo_sala
+			})
+		}
+	}
 
-  insert = async ({ _id, codigo_sala, numero_piso, titulo_bloco, titulo_campus, titulo_sala, _rev }) => {
-    if(_rev){
-      response = await database.put({
-        _id, _rev, codigo_sala, numero_piso, titulo_bloco, titulo_campus, titulo_sala
-      })
-    }else{
-      await database.put({
-        _id, codigo_sala, numero_piso, titulo_bloco, titulo_campus, titulo_sala
-      })
-    }
-  }
+	getAllDocsID = async () => {
+		const response = await database.allDocs({
+			include_docs: false,
+			attachments: false
+		})
+		const docs = response.rows.filter(item => item.id !== "updateVersionID")
+		const docsID = docs.map(item => item.id)
+		return docsID
+	}
 
-  getAllDocsID = async () => {
-    const response = await database.allDocs({
-      include_docs: false,
-      attachments: false,
-    })
-    const docs = response.rows.filter(item => item.id !== "updateVersionID")
-    const docsID = docs.map(item => item.id )
-    return docsID
-  }
+	getAllDocs = async () => {
+		const rawDocs = await database.allDocs({
+			include_docs: true,
+			attachments: false
+		})
+		let docs = rawDocs.rows.filter(item => item.doc._id !== "versionCode")
+		docs = docs.map(item => item.doc)
+		return docs
+	}
 
-  getAllDocs = async () => {
-    const rawDocs = await database.allDocs({
-      include_docs: true,
-      attachments: false,
-    })
-    let docs = rawDocs.rows.filter(item => item.doc._id !== "versionCode")
-    docs = docs.map(item => item.doc)
-    return docs
-  }
+	setData = async (docsApi, versionCode, func) => {
+		try {
+			if (docsApi) {
+				switch (func) {
+					case "update":
+						try {
+							const docsDB = await getAllDocs()
+							const differences = await arraySync(docsDB, docsApi, "_id")
 
-  setData = async (docsAPI, versionCode, func) => {
-    try{
-      if (docsAPI){
-        switch(func){
-          case 'update':
-            try{
-              const docsDB = await getAllDocs()
-              const differences = await arraySync(docsDB, docsAPI, '_id')
-              
-              differences.removed.forEach(async item => {
-                  await database.remove(item._id, item._rev)      
-              }) 
-              differences.created.forEach(async item => {
-                  await insert(item)
-              })
-              differences.changed.forEach(async updateItem => {
-                  sourceItem = await database.get(updateItem._id)
-                  updateItem._rev = sourceItem._rev
-                  await insert(updateItem)
-              })
-              
-            }catch(error){
-              throw new Error(error)
-            }
-          break
-          case 'insert':
-            docsAPI.forEach(async item => {
-              insert(item) 
-            })
-          break 
-        }
-        
-      }else{
-        throw new Error("Sem dados para inserir")
-      }
-      if(versionCode){
-        switch(func){
-          case 'update':
-            versionCodeDB = await database.get('versionCode')
-            database.put({
-              _id: 'versionCode',
-              _rev: versionCodeDB._rev,
-              versionCode: versionCode
-            })
-            break
-          case 'insert':
-            database.put({
-              _id: 'versionCode',
-              versionCode: versionCode
-            })
-            .then(message => console.log(message))
-            .catch(error => console.log(error))
-          break
-        }
-      }else{
-        throw new Error("Sem 'versionCode' para inserir")
-      }
-    }catch(error){
-      throw('Erro: '+error)
-    }
-  }
+							differences.removed.forEach(async item => {
+								await database.remove(item._id, item._rev)
+							})
+							differences.created.forEach(async item => {
+								await insert(item)
+							})
+							differences.changed.forEach(async updateItem => {
+								sourceItem = await database.get(updateItem._id)
+								updateItem._rev = sourceItem._rev
+								await insert(updateItem)
+							})
+						} catch (error) {
+							throw error
+						}
+						break
+					case "insert":
+						docsApi.forEach(async item => {
+							insert(item)
+						})
+						break
+				}
+			} else {
+				throw "Sem dados para inserir"
+			}
+			if (versionCode) {
+				switch (func) {
+					case "update":
+						versionCodeDB = await database.get("versionCode")
+						database.put({
+							_id: "versionCode",
+							_rev: versionCodeDB._rev,
+							versionCode: versionCode
+						})
+						break
+					case "insert":
+						database
+							.put({
+								_id: "versionCode",
+								versionCode: versionCode
+							})
+							.then(message => console.log(message))
+							.catch(error => console.log(error))
+						break
+				}
+			} else {
+				throw "Sem 'versionCode' para inserir"
+			}
+		} catch (error) {
+			throw "Erro: " + error
+		}
+	}
 
-  migration = async () => {
-    docs = await database.allDocs({
-      include_docs: false,
-      attachments: false,
-    })
-    console.log(docs.total_rows)
-    if(docs.total_rows<=0){
-      const {data, versionCode} = await APIConn.setData()
-      docs = data
-      await setData(docs, versionCode, 'insert')
-    }
-  }
+	migration = async () => {
+		docs = await database.allDocs({
+			include_docs: false,
+			attachments: false
+		})
+		if (docs.total_rows <= 0) {
+			const { data, versionCode } = await apiConn.setData()
+			docs = data
+			await setData(docs, versionCode, "insert")
+		}
+	}
 
-  update = async (docs, versionCode) => {
-    await setData(docs, versionCode, 'update')
-  }
+	update = async (docs, versionCode) => {
+		await setData(docs, versionCode, "update")
+	}
 
-  checkUpdate = async () => {
-
-    const { versionCode} = await APIConn.setData(false, true)
-    versionCodeAPI = versionCode
-    versionCodeDB = await database.get('versionCode')
-    versionCodeDB = versionCodeDB.versionCode
-    console.log(versionCodeAPI, versionCodeDB)
-    if (versionCodeAPI > versionCodeDB){
-      const { data } = await APIConn.setData(true, false)
-      docs = data
-      await update(docs, versionCodeAPI)
-    }
-  }
-  return{ migration, checkUpdate, getAllDocs}
+	checkUpdate = async () => {
+		const { versionCode } = await apiConn.setData(false, true)
+		versionCodeApi = versionCode
+		versionCodeDB = await database.get("versionCode")
+		versionCodeDB = versionCodeDB.versionCode
+		console.log(versionCodeApi, versionCodeDB)
+		if (versionCodeApi > versionCodeDB) {
+			const { data } = await apiConn.setData(true, false)
+			docs = data
+			await update(docs, versionCodeApi)
+		}
+	}
+	return { migration, checkUpdate, getAllDocs }
 }
 
 module.exports = Database
