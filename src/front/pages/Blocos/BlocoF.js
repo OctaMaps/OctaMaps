61cf637a5a951c84e3ebe3b77a3ba50102304260
SnowLew <aroldogooulart@gmail.com>
2020-05-10 14:24:21 -0400

    Atualiar expo

--- src/front/pages/Blocos/BlocoF.js
+++ src/front/pages/Blocos/BlocoF.js
@@ -1,79 +1,75 @@
-import React from 'react';
-import { StyleSheet, View, ImageBackground } from 'react-native';
+import React from "react";
+import { StyleSheet, View, ImageBackground } from "react-native";
 
-import { Video } from 'expo-av';
+import { Video } from "expo-av";
 
-import Header from '../../../component/NewHeader'
+import Header from "../../../component/NewHeader";
 
-import MapF from '../../../component/maps/MapF'
-import Info from '../../../component/text/TF'
+import MapF from "../../../component/maps/MapF";
+import Info from "../../../component/text/TF";
 export default class BlocoF extends React.Component {
-  constructor(props){         
+  constructor(props) {
     super(props);
     this.state = {
-      videoOn: false
+      videoOn: false,
     };
   }
 
-   componentWillMount() {
-    const {navigation} = this.props;
+  UNSAFE_componentWillMount() {
+    const { navigation } = this.props;
     //Quando o componente for pressionado, permita a exibição do video
-    navigation.addListener('willFocus',
-      ()=>{
-        this.setState({videoOn:true})
-        console.log("Video Permetido.")
-        console.log(this.state.videoOn)
-        }
-    )
+    navigation.addListener("willFocus", () => {
+      this.setState({ videoOn: true });
+      console.log("Video Permetido.");
+      console.log(this.state.videoOn);
+    });
   }
 
   //Funçoes executadas no Video
-  onError(err){
-    console.log("Error Happen: ", err)
+  onError(err) {
+    console.log("Error Happen: ", err);
   }
 
-  onLoadStart(){
-    console.log("Carregando Video.")
+  onLoadStart() {
+    console.log("Carregando Video.");
   }
 
-  onLoad(){
-    setTimeout( () => {
-       console.log("Video Terminado")
-       this.setState({videoOn: false})
-    },1300)
+  onLoad() {
+    setTimeout(() => {
+      console.log("Video Terminado");
+      this.setState({ videoOn: false });
+    }, 1300);
   }
-
 
   render() {
     return (
       <View style={styles.container}>
-       <ImageBackground source={this.state.videoOn ? require('../../images/Index.png') : require('../../images/BlocoF.png')} style={{width: '100%', height: '100%'}}>
-        { this.state.videoOn 
-          ? 
-            <Video
-                  source={require('../../video/BlocoF.mp4')}
-                  rate={1.0}
-                  volume={1.0}
-                  isMuted={false}
-                  resizeMode="cover"
-                  shouldPlay
-                  isLooping={false}
-                  onError={(err) => this.onError(err)}
-                  onLoad={() => this.onLoad()}
-                  onLoadStart={() => this.onLoadStart()}
-                  style={{ width: '100%', height: '100%' }}
-
-              />
-            :
-              null
+        <ImageBackground
+          source={
+            this.state.videoOn
+              ? require("../../images/Index.png")
+              : require("../../images/BlocoF.png")
           }
-          <Header searchableOff/>
-         <MapF/>
-           
-       </ImageBackground> 
-      
-
-          
+          style={{ width: "100%", height: "100%" }}
+        >
+          {this.state.videoOn ? (
+            <Video
+              source={require("../../video/BlocoF.mp4")}
+              rate={1.0}
+              volume={1.0}
+              isMuted={false}
+              resizeMode="cover"
+              shouldPlay
+              isLooping={false}
+              onError={(err) => this.onError(err)}
+              onLoad={() => this.onLoad()}
+              onLoadStart={() => this.onLoadStart()}
+              style={{ width: "100%", height: "100%" }}
+            />
+          ) : null}
+          <Header searchableOff />
+          <MapF />
+        </ImageBackground>
       </View>
     );
   }
@@ -81,17 +77,16 @@ export default class BlocoF extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1
-  }
+    flex: 1,
+  },
 });
 
 const header = StyleSheet.create({
-  header:{
-    height:65,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    backgroundColor: 'transparent'
-  }
+  header: {
+    height: 65,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "transparent",
+  },
 });
-
