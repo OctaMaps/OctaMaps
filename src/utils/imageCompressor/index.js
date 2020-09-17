0251425c9e8822c3cfefaa933917b426ed1938b6
SnowLew <aroldogooulart@gmail.com>
2020-09-17 20:40:58 -0300

    feat: script de otimizacao de imagens

---
+++ src/utils/imageCompressor/index.js
@@ -0,0 +1,35 @@
+// This is code will compress files of images
+// Doc: https://github.com/semiromid/compress-images
+// This optimize the image size of archives, run this every you need
+// To execute "yarn optimize"
+
+const compress_images = require("compress-images")
+
+let inputPath;
+let outputPath;
+let errors = [];
+
+inputPath = "../../images/*/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}";
+outputPath = "../../optimizedImages";
+
+compress_images(
+    inputPath, 
+    outputPath, 
+    { compress_force: false, statistic: true, autoupdate: true }, false,
+    { jpg: { engine: "mozjpeg", command: ["-quality", "60"] } },
+    { png: { engine: "pngquant", command: ["--quality=20-50", "-o"] } },
+    { svg: { engine: "svgo", command: "--multipass" } },
+    { gif: { engine: "gifsicle", command: ["--colors", "64", "--use-col=web"] } },
+
+  function (error, completed, statistic) {
+    if(statistic) {
+        console.log("-------------");
+        console.log(statistic);
+        console.log("-------------");
+    }   
+    if(error) 
+        errors.push(error)
+  } 
+);
+
+console.log(errors ? "Everything was ok, no errors found." : `Error happened: ${errors} `)
